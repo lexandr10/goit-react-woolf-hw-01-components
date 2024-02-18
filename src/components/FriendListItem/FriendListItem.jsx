8a15df643ae4c2b00cdda3cddfdffe9ad138bde1
lexandr10 <alexandrenko3006@gmail.com>
2024-02-18 14:30:19 -0500

    change frienlist

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,10 @@
 import stl from './FriendListItem.module.css';
-export const FriendListItem = ({ listFriends }) => {
-  return listFriends.map(({ avatar, name, isOnline, id }) => (
+export const FriendListItem = ({ persons: { id, name, avatar, isOnline } }) => {
+  return (
     <li className={stl.item} key={id}>
       <span className={isOnline ? stl.online : stl.offline}></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
-  ));
+  );
 };
--- src/components/friends.list/friends.jsx
+++ src/components/friends.list/friends.jsx
@@ -1,7 +1,11 @@
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import stl from './friends.module.css';
-export const Friends = ({ listFriends }) => (
-  <ul className={stl.list}>
-    <FriendListItem listFriends={listFriends}></FriendListItem>
-  </ul>
-);
+export const Friends = ({ listFriends }) => {
+  return (
+    <ul className={stl.list}>
+      {listFriends.map(person => (
+        <FriendListItem persons={person}></FriendListItem>
+      ))}
+    </ul>
+  );
+};
